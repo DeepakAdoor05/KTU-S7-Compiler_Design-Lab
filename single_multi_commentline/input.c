7f4643cc602f97252010409e0180945f33317d2b
Deepak <deepak@gmail.com>
2024-11-12 23:52:08 +0530

    All codes added

---
+++ single_multi_commentline/input.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/* This program is to print Hello World */
+ int main() // main function
+ {
+    printf("Hello, World!"); // printing hello world
+ }
+ /* End of the program */
+ 
